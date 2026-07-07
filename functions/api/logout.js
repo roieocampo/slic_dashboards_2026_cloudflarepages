f1f613dd492dd3d4a881fb26ba17cdd7356df00d
roieocampo <lenredroieocampo@gmail.com>
2026-07-07 13:06:14 +0800

    Delete functions directory

--- functions/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { json } from '../_utils.js';
-export async function onRequestPost() {
-  return json({ ok: true }, 200, {
-    'set-cookie': 'slic_cf_session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0',
-  });
-}
